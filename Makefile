58737c72920ee57f30432084cdc21caa91eb4c5c
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2024-09-24 05:17:51 +0800

    Add 'foo' and 'bar' C samples (placeholder)

--- Makefile
+++ Makefile
@@ -6,10 +6,16 @@ COMMON_SRC = samples/common/window.c
 
 .PHONY: all clean
 
-all: hello-world
+all: hello-world foo bar
 
 hello-world: samples/hello-world/main.c $(COMMON_SRC)
 	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
 
+foo: samples/foo/main.c $(COMMON_SRC)
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
+bar: samples/bar/main.c $(COMMON_SRC)
+	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS)
+
 clean:
-	rm -f hello-world
+	rm -f hello-world foo bar
---
+++ samples/bar/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <GLES3/gl3.h>
+#include <EGL/egl.h>
+#include <GLFW/glfw3.h>
+#include "window.h"
+
+int main() {
+    GLFWwindow* window;
+    EGLDisplay display;
+    if (initializeWindow(&window, &display, 640, 480, "OpenGL Samples - Bar") != 0) {
+        return -1;
+    }
+
+    // To be written.
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // To be written.
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    terminateWindow(window);
+
+    return 0;
+}
