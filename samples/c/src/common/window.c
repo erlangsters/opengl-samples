b5be92f065b518c18385ecc3ceb79b9ec1872a70
Jonathan De Wachter <dewachter.jonathan@gmail.com>
2024-10-23 22:33:09 +0800

    Add LICENSE.md and copyright header to source files

--- samples/c/src/common/window.c
+++ samples/c/src/common/window.c
@@ -1,3 +1,12 @@
+//
+// Copyright (c) 2025, Byteplug LLC.
+//
+// This source file is part of a project made by the Erlangsters community and
+// is released under the MIT license. Please refer to the LICENSE.md file that
+// can be found at the root of the project repository.
+//
+// Written by Jonathan De Wachter <jonathan.dewachter@byteplug.io>
+//
 #include "window.h"
 #include <stdio.h>
 
